26c75148f6bad7f8b9d1de5b86896f3a4f116df3
cereia <104402370+cereia@users.noreply.github.com>
2024-06-24 16:09:53 -0400

    Add custom enumerables using blocks

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,62 @@
+# frozen_string_literal: true
+
+# custom enumerables
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    my_each do |element|
+      yield element, index
+      index += 1
+    end
+    self
+  end
+
+  def my_select
+    selected = []
+    my_each { |element| selected << element if yield element }
+    selected
+  end
+
+  def my_all?
+    all = true
+    my_each { |element| all = false unless yield element }
+    all
+  end
+
+  def my_any?
+    any = false
+    my_each { |element| any = true if yield element }
+    any
+  end
+
+  def my_none?
+    none = true
+    my_each { |element| none = false if yield element }
+    none
+  end
+
+  def my_count
+    i = 0
+    if block_given?
+      my_each { |element| i += 1 if yield element }
+      i
+    else
+      length
+    end
+  end
+
+  def my_map
+    mapped = []
+    my_each { |element| mapped << yield(element) }
+    mapped
+  end
+
+  def my_inject(initial_value)
+    result = initial_value
+    my_each { |element| result = yield(result, element) }
+    result
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +65,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return unless block_given?
+
+    length.times { |i| yield self[i] }
+    self
+  end
 end
